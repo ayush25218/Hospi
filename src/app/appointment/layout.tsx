5c02c0238d1fa933a83755ad98ff2e471c65cf6d
sushant12233 <royalsushant574@gmail.com>
2025-11-03 23:34:31 +0530

    sidebar done

---
+++ src/app/appointment/layout.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { useState } from 'react';
+import { Sidebar } from "@/app/sidebar";
+export default function appointmentayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClose = () => setIsOpen(false);
+
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      {/* Sidebar */}
+      <Sidebar isOpen={isOpen} onClose={handleClose} />
+
+      {/* Main Page Content */}
+      <main className="flex-1 p-6 overflow-y-auto">{children}</main>
+    </div>
+  );
+}
