9ca61392aca2885211627512529fdfb812e3a6c0
sushant12233 <royalsushant574@gmail.com>
2025-11-05 18:57:10 +0530

    sidebar fixed

---
+++ src/app/settings/layout.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { useState } from 'react';
+import { Sidebar } from "@/app/sidebar";
+
+export default function settingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      {/* Sidebar */}
+      <Sidebar isOpen={isOpen} onClose={() => setIsOpen(false)} />
+
+      {/* Main Page Content */}
+      <main className="flex-1 p-6 overflow-y-auto">{children}</main>
+    </div>
+  );
+}
